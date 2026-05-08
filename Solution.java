3b7a153492ab98e073913c9386f1fe33e6c7f208
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-05-08 19:21:04 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<Character>();
+        int l = 0;
+        int max = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(set.contains(s.charAt(i))){
+                while(set.contains(s.charAt(i))){
+                    set.remove(s.charAt(l++));
+                }
+            }
+
+            set.add(s.charAt(i));
+            max = Math.max(max, set.size());
+        }
+
+        return(max);
+    }
+}
